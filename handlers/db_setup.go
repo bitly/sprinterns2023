f4cd264d81d75b99bf8a7cacf6afe80dcd75b8ad
dianaabishop <dianaabishop@users.noreply.github.com>
2022-12-31 14:16:55 -0500

    db name (#2)

--- handlers/db_setup.go
+++ handlers/db_setup.go
@@ -12,7 +12,7 @@ import (
 var dbmap = initDb()
 
 func initDb() *gorp.DbMap {
-	db, err := sql.Open("mysql", "root:admin@tcp(localhost:3306)/retro")
+	db, err := sql.Open("mysql", "root:admin@tcp(localhost:3306)/events")
 	checkErr(err, "sql.Open failed")
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
 	err = dbmap.CreateTablesIfNotExists()
