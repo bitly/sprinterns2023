18acca04698cd98e515dc827f76b81cb45aee831
dianaabishop <dianaabishop@users.noreply.github.com>
2023-01-04 08:43:24 -0500

    Update run_sql_scripts.sh

--- run_sql_scripts.sh
+++ run_sql_scripts.sh
@@ -2,4 +2,4 @@
 # This bash script loops through the sql directory and applies the sql to our db
 
 cd sql
-for FILE in *; do mysql -u root -p"admin" < $FILE; done
+for FILE in *; do mysql -u root -p"admin123" < $FILE; done
