c73e329738efd4f0f8b0e11d07c6dea29f092a59
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-06 10:37:18 -0500

    syntax fixed

--- sql/create_rsvp_table.sql
+++ sql/create_rsvp_table.sql
@@ -1,13 +1,11 @@
 use events;
-
-CREATE TABLE IF NOT EXISTS 'rsvp' (
-       'response_id' integer PRIMARY KEY AUTO_INCREMENT.
-       'event_id' integer NOT NULL,
-       'name' text NOT NULL,
-       'rsvp' text NOT NULL, /*yes, no, maybe*/
-       'comment_id' text, 
-       'created_at' timestamp NOT NULL default CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS `rsvp` (
+       `response_id` integer PRIMARY KEY AUTO_INCREMENT,
+       `event_id` integer NOT NULL,
+       `name` text NOT NULL,
+       `rsvp` text NOT NULL,
+       `comment_id` text, 
+       `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+       FOREIGN KEY (event_id) REFERENCES event(event_id)
 ) DEFAULT CHARSET=utf8;
-       
-       
        
