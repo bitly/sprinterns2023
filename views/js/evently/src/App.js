288a2d137252324a7f70a810252e6ae25eea6222
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-09 17:09:37 -0500

    Code from Diana

--- views/js/evently/src/App.js
+++ views/js/evently/src/App.js
@@ -1,19 +1,34 @@
-import logo from './logo.svg';
-import CreateEvent from './CreateEvent.jsx'; 
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import HomePage from './HomePage.jsx'; 
+import CreateEvent from './CreateEvent.jsx'; 
 import RSVP from './RSVP.jsx'; 
 //import ViewEvent from './ViewEvent.jsx'; 
 //Add About Us page
 //Add a community page
 
-
 function App() {
   return (
-    <> 
-    <CreateEvent /> 
-    <HomePage/>
-    <RSVP/>
-    </>
+    <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/HomePage.jsx">Home</Link>
+        </li>
+        <li>
+          {/* <Link to="/create-event">Create Event</Link> */}
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/create-event" element={<CreateEvent />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </div>
   );
 }
 
