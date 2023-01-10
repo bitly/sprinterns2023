382a0ed8a4ae37cf6930086675dfc3ce0261f083
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-10 09:58:56 -0500

    Logo and title better aligned

--- views/js/evently/src/HomePage.jsx
+++ views/js/evently/src/HomePage.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 import './HomePage.css'
+import logo from './bitly-logo.png';
 
 const HomePage: React.FC = () => {
     return (
         <div className = "App HomePage">
-            <img src="https://seeklogo.com/images/B/bitly-logo-BFC8CAF4A0-seeklogo.com.png" alt="Bitly Logo"></img>
+            <img src={logo} alt="Bitly Logo"></img>
             <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center">
                 <h1 class = "Title">Event.ly</h1>
-                <p class = "Text">Host your events here!</p>
+                <h2 class = "Text">Host your events here!</h2>
                 <button>Get Started!</button>
                 <b></b>
-                <p>Find an event!</p>
+                <h2 class = "Text">Find an event!</h2>
                 <button>RSVP</button>
             </div>
         </div>
