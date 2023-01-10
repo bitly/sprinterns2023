0ed740cbabb3ab345c9094f6317b1fc2c139ec11
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-10 09:43:55 -0500

    Logo added

--- views/js/evently/src/HomePage.jsx
+++ views/js/evently/src/HomePage.jsx
@@ -4,6 +4,7 @@ import './HomePage.css'
 const HomePage: React.FC = () => {
     return (
         <div className = "App HomePage">
+            <img src="https://seeklogo.com/images/B/bitly-logo-BFC8CAF4A0-seeklogo.com.png" alt="Bitly Logo"></img>
             <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center">
                 <h1 class = "Title">Event.ly</h1>
                 <p class = "Text">Host your events here!</p>
