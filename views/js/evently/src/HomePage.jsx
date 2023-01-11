98824b7cc9acad6d149e08862539f56a0be343e1
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-11 15:30:32 -0500

    Buttons linked to pages

--- views/js/evently/src/HomePage.jsx
+++ views/js/evently/src/HomePage.jsx
@@ -1,15 +1,23 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css'
 import './HomePage.css'
+import CreateEvent from './CreateEvent.jsx'; 
 import logo from './bitly-logo.png';
 
 const HomePage: React.FC = () => {
+    
     return (
         <div className = "App HomePage">
             <img src={logo} alt="Bitly Logo"></img>
             <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center" id="MainText">
                 <h1 class = "Title">Event.ly</h1>
                 <h2 class = "Text">Host your events here!</h2>
-                <button>Get Started!</button>
+                <Link to="/create-event" class="buttons">Get Started</Link>
                 <b></b>
                 <h2 class = "Text">Find an event!</h2>
                 <button>RSVP</button>
