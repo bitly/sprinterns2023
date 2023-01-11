21e4f6109d4317da86c7d22c25d2a9c307b42b1f
Sharron Qian <sharronsq@gmail.com>
2023-01-11 10:14:49 -0500

    Finished header and formatting final touches

--- views/js/evently/src/CreateEvent.jsx
+++ views/js/evently/src/CreateEvent.jsx
@@ -8,90 +8,82 @@ function CreateEvent() {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
-
     return(
       <div className="wrapper"> 
         <Header/>
-        {/* <fieldset style={{ backgroundColor: 'rgba(238, 97, 35)' }}>
-          <h1>Event.ly</h1> 
-        </fieldset> */}
+        <div className = "container">
+          <div className = "form-container">
+            <h3> Enter the details of your event:</h3>
+             {/* Date form */}
+            <form> 
+            <fieldset> {/* Creates box around the text and input box */}
+              <p>Date</p>
+              <input date="date" /> {/* Creates a box that takes an input, saves it into date */}
+            </fieldset> <br></br>
 
-        
+            {/* Time form */}
+            <fieldset>
+                <p>Time</p>
+                <input time="time" />
+            </fieldset> <br></br>
 
+            {/* Location form */}
+            <fieldset>
+                <p>Location</p>
+                <input location="location" />
+            </fieldset> <br></br>
 
-        {/* Date form */}
-        <form> 
-        <fieldset width="10"> {/* Creates box around the text and input box */}
-          <p>Date</p>
-          <input date="date" /> {/* Creates a box that takes an input, saves it into date */}
-        </fieldset> <br></br>
-
-        {/* Time form */}
-        <fieldset>
-            <p>Time</p>
-            <input time="time" />
-         </fieldset> <br></br>
-
-        {/* Upload an image */}
-        {/* fieldset style creates a gray box */}
-        <ul class="tableB">
-          <li>
-            <fieldset style={{ backgroundColor: 'rgba(217, 217, 217)', padding: '100px'}}>
-              <h2> Choose an image:</h2>
-              {/* Takes a file as input; onChange passes value to handleChange */}
-              <input type = "file" onChange={handleChange} />
-              <img src = {file} />
-            </fieldset>
-          </li>
-        </ul>
-
-        {/* Location form */}
-        <fieldset>
-            <p>Location</p>
-            <input location="location" />
-        </fieldset> <br></br>
-
-        {/* Description form */}
-        <fieldset>
-            <p>Description</p>
-            <input description="description" />
-        </fieldset> <br></br>
+            {/* Description form */}
+            <fieldset>
+                <p>Description</p>
+                <input description="description" />
+            </fieldset> <br></br>
 
 
-        {/* # of attendees form */}
-        <fieldset>
-            <p># of attendees</p>
-            <input numAttendees="numAttendees" />
-        </fieldset> <br></br>
+            {/* # of attendees form */}
+            <fieldset>
+                <p># of attendees</p>
+                <input numAttendees="numAttendees" />
+            </fieldset> <br></br>
 
-        {/* public/private form */}
-        <fieldset>
-             <p>Public/Private</p>
-             <input isPrivate ="isPrivate" />
-         </fieldset> <br></br>
+            {/* public/private form */}
+            <fieldset>
+                <p>Public/Private</p>
+                <input isPrivate ="isPrivate" />
+            </fieldset> <br></br>
 
-        {/* Host name form */}
-        <fieldset>
-            <p>Host name</p>
-            <input hostName="hostName" />
-         </fieldset> <br></br>
+            {/* Host name form */}
+            <fieldset>
+                <p>Host name</p>
+                <input hostName="hostName" />
+            </fieldset> <br></br>
 
-        {/* Contact info form */}
-        <fieldset>
-            <p>Contact info</p>
-            <input contact="contact" />
-         </fieldset> <br></br>
+            {/* Contact info form */}
+            <fieldset>
+                <p>Contact info</p>
+                <input contact="contact" />
+            </fieldset> <br></br><br></br>
 
-        <fieldset>
-          {/* Button to publish draft */}
-          <button type="publish">Publish</button>
+            <div>
+              {/* Button to publish draft */}
+              <button type="publish" className = "publish-button">Publish</button>
 
-          {/* Button to save draft (if have time) */}
-          <button type="save draft">Save draft</button>
-        </fieldset>
-        
-        </form>
-        
+              {/* Button to save draft (if have time) */}
+              <button type="save draft" className = "save-button">Save draft</button>
+            </div>
+            </form>
+          </div>
+          <div className='upload-container'>
+            {/* Upload an image */}
+            {/* fieldset style creates a gray box */}
+                <fieldset style={{ backgroundColor: 'rgba(217, 217, 217)', padding: '200px'}}>
+                  <h2> Choose an image:</h2>
+                  {/* Takes a file as input; onChange passes value to handleChange */}
+                  <input type = "file" onChange={handleChange} />
+                  <img src = {file} />
+                </fieldset>
+          </div>
+        </div>
       </div>
     )
   }
--- views/js/evently/src/header.js
+++ views/js/evently/src/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './bitly-logo.svg';
+import logo from './bitly-logo-white.png';
 import './header.css';
 
 function Header() {
   return (
     <div className="Header">
-            <img width="100" height="100" src = {logo}></img>
-            <h1>Evently</h1>
+      <div className="logo">
+        <img width="50" height="50" src = {logo}></img>
+      </div>
+      <h1>Evently</h1>
     </div>
   );
 }
