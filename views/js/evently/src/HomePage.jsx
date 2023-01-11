b5237759af1d976a97c0912c520cf3820ddaa52f
umidar <umida.ravshanova03@gmail.com>
2023-01-11 13:32:41 -0500

    Basic styling done

---
+++ views/js/evently/src/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css'
+import './HomePage.css'
+import logo from './bitly-logo.png';
+
+const HomePage: React.FC = () => {
+    return (
+        <div className = "App HomePage">
+            <img src={logo} alt="Bitly Logo"></img>
+            <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center" id="MainText">
+                <h1 class = "Title">Event.ly</h1>
+                <h2 class = "Text">Host your events here!</h2>
+                <button>Get Started!</button>
+                <b></b>
+                <h2 class = "Text">Find an event!</h2>
+                <button>RSVP</button>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage
