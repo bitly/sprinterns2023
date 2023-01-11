e16bd4f77fd3dde4ee4552d97736aaa5c97ac5fd
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-11 10:22:58 -0500

    endpoint adds data to database

--- handlers/events.go
+++ handlers/events.go
@@ -29,6 +29,15 @@ func CreateEvent(c *gin.Context) {
 	if err := c.BindJSON(&event); err != nil {
 		return
 	}
+
+	_, err := dbmap.Query(
+		"INSERT INTO event (title, date, time, location, host_name, description, contact_info, public_private, num_of_RSVP, max_attendees) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+		event.EventTitle, event.Date, event.Time, event.Location, event.HostName, event.Description, event.ContactInfo, event.PublicPrivate, event.NumRSVP, event.MaxAttendees)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	fmt.Println(event)
 
 	c.JSON(201, event)
