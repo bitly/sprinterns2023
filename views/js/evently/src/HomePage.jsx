3f6747d12d977d52d33295f3306eb883fb410991
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-11 16:49:12 -0500

    Create event button linked to page

--- views/js/evently/src/HomePage.jsx
+++ views/js/evently/src/HomePage.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css'
 import './HomePage.css'
+import CreateEvent from './CreateEvent.jsx'; 
 import logo from './bitly-logo.png';
 
 const HomePage: React.FC = () => {
@@ -9,7 +16,7 @@ const HomePage: React.FC = () => {
             <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center" id="MainText">
                 <h1 class = "Title">Event.ly</h1>
                 <h2 class = "Text">Host your events here!</h2>
-                <button>Get Started!</button>
+                <Link to="/create-event" class="buttons">Get Started</Link>
                 <b></b>
                 <h2 class = "Text">Find an event!</h2>
                 <button>RSVP</button>
