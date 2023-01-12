b9ef29698e3f2f9b059995309075d831da2fad7f
noshin07 <noshin7201@gmail.com>
2023-01-12 13:21:22 -0500

    Cleaned up the files.

--- handlers/events.go
+++ handlers/events.go
@@ -20,7 +20,7 @@ func CreateEvent(c *gin.Context) {
 	_, err := dbmap.Query(
 		"INSERT INTO event (title, date, time, location, host_name, description, contact_info, public_private, num_of_RSVP, max_attendees) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
 		event.EventTitle, event.Date, event.Time, event.Location, event.HostName, event.Description, event.ContactInfo, event.PublicPrivate, event.NumRSVP, event.MaxAttendees)
-		
+
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, nil) //server error
 		return
@@ -37,17 +37,17 @@ func GetEvent(c *gin.Context) {
 		see_row)
 
 	fmt.Printf("%+v", eventrow)
-	
+
 	for eventrow.Next() {
 		var event models.GetEvent
-        // for each row, scan into the event struct
+		// for each row, scan into the event struct
 		err = eventrow.Scan(&event.EventID, &event.EventTitle, &event.Date, &event.Time, &event.Location, &event.HostName, &event.Description, &event.ContactInfo, &event.PublicPrivate, &event.NumRSVP, &event.MaxAttendees)
 		if err != nil {
-		fmt.Println(err)
-		c.IndentedJSON(http.StatusInternalServerError, nil) //server error
-		return
-	}
-        // append the event into events array
+			fmt.Println(err)
+			c.IndentedJSON(http.StatusInternalServerError, nil) //server error
+			return
+		}
+		// append the event into events array
 		events = append(events, event)
 	}
 	c.JSON(201, events) //success
@@ -76,26 +76,23 @@ func CreateRSVP(c *gin.Context) {
 }
 
 func GetRSVP(c *gin.Context) {
-	var rsvp1 []models.GetRSVP
+	var rsvpList []models.GetRSVP
 
 	see_row := c.Param("responseID")
 	RSVProw, err := dbmap.Query(
 		"SELECT response_id, event_id, name, rsvp, comment_id FROM RSVP WHERE response_id=?;",
-		see_row)
+		seeRow)
 
-	fmt.Printf("%+v", RSVProw)
-
-for RSVProw.Next() {
-	var rsvp models.GetRSVP
-	// for each row, scan into the event struct
-	err = RSVProw.Scan(&rsvp.ResponseID, &rsvp.EventID, &rsvp.Name, &rsvp.RSVP, &rsvp.CommentID)
-	if err != nil {
-	fmt.Println(err)
-	c.IndentedJSON(http.StatusInternalServerError, nil) //server error
-	return
-}
-	// append the event into events array
-	rsvp1 = append(rsvp1, rsvp)
-}
+	for RSVProw.Next() {
+		var rsvp models.GetRSVP
+		// for each row, scan into the event struct
+		err = RSVProw.Scan(&rsvp.ResponseID, &rsvp.EventID, &rsvp.Name, &rsvp.RSVP, &rsvp.CommentID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, nil) //server error
+			return
+		}
+		// append the event into events array
+		rsvpList = append(rsvpList, rsvp)
+	}
 	c.JSON(201, rsvp1) //success
 }
