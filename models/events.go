992ae2befd9941dcb28f3a5c80e9e0ee45bb7e37
umidar <umida.ravshanova03@gmail.com>
2023-01-13 11:29:14 -0500

    Null commentID

--- models/events.go
+++ models/events.go
@@ -45,5 +45,5 @@ type GetRSVP struct {
 	EventID       int    `json:"event_id"`
 	Name          string `json:"name`
 	RSVP          string `json:"rsvp" `
-	CommentID     int    `json:"comment_id" `
+	CommentID     *int    `json:"comment_id" `
 }
