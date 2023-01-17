974ac90b74a98d492e1d0da2e74131e32a5300d8
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2023-01-17 15:56:55 -0500

    Headers added to pages

--- views/js/evently/src/CommunityPage.jsx
+++ views/js/evently/src/CommunityPage.jsx
@@ -1,8 +1,12 @@
 import './CommunityPage.css'
+import Header from './header.js'
 
 function CommunityPage() {
     return(
-        <h1>In progress!</h1>
+        <div>
+            <Header/>
+            <h1>In progress!</h1>
+        </div>
     )
 }
 
--- views/js/evently/src/HomePage.jsx
+++ views/js/evently/src/HomePage.jsx
@@ -6,14 +6,16 @@ import './App.css'
 import './HomePage.css'
 import CreateEvent from './CreateEvent.jsx'
 import CommunityPage from './CommunityPage.jsx'
-import logo from './bitly-logo.png';
+import Header from './header.js'
+// import logo from './bitly-logo.png';
 
 const HomePage: React.FC = () => {
     return (
         <div class = "HomePage">
-            <img src={logo} alt="Bitly Logo" id="HomeLogo"></img>
+            <Header/>
+            {/* <img src={logo} alt="Bitly Logo" id="HomeLogo"></img> */}
             <div className = "col-xs-8 col-xs-offset-2 jumbotron text-center" id="MainText">
-                <h1 class = "Title">Event.ly</h1>
+                {/* <h1 class = "Title">Event.ly</h1> */}
                 <h2 class = "Text">Host your events here!</h2>
                 <Link to="/create-event" class="buttons">Get Started</Link>
                 <b></b>
