b0de7e0ea0dc7ecbad4742b7b3bc23ffe35213b4
Sharron Qian <sharronsq@gmail.com>
2023-01-17 16:26:26 -0500

    Updated page to include placeholders for text

--- views/js/evently/src/ViewEvent.jsx
+++ views/js/evently/src/ViewEvent.jsx
@@ -9,18 +9,26 @@ function ViewEvent() {
             <Header/>
             <div className="container">
                 <div className="info-container">
-                    <fieldset className="title">
-                        <h1> [Title] </h1>
-                    </fieldset>
+                    <div className='title'> 
+                        <h1>Title</h1> 
+                    </div>
+                    <h3>[Time and date]</h3>
+                    <h3>[Description]</h3>
+                    <h3>[Max attendees]</h3>
+                    <br></br>
+                    <h3>[Host name]</h3>
+                    <h3>[Contact info]</h3>
+                    <br></br>
                 </div>
                 <div className="image-container">
                     <img src={placeholder} className="image"></img>
                 </div>
             </div>
-            <button className="rsvp-button">
-                RSVP here
-            </button>
-            
+            <div className='rsvp-button-container'>
+                <button className="rsvp-button">
+                    RSVP here
+                </button>
+            </div>
         </div>
         
     )
