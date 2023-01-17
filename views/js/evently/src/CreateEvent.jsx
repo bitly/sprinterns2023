8396100225fc31b5bb677a2b0d033fad5f9603a3
noshin07 <noshin7201@gmail.com>
2023-01-17 10:04:18 -0500

    Connected create api to create page.
    Trying to get POST working.
    
    Made changes to run api.
    
    Made changes to make it work.

--- views/js/evently/src/CreateEvent.jsx
+++ views/js/evently/src/CreateEvent.jsx
@@ -4,75 +4,109 @@ import Header from './header.js';
 
 function CreateEvent() {
   const [file, setFile] = useState();
+  const [EventTitle, setEventTitle ] = useState("");
+  const [DateForm, setDateForm] = useState("");
+  const [TimeForm, setTimeForm] = useState("");
+  const [LocationForm, setLocationForm] = useState("");
+  const [DescriptionForm, setDescriptionForm] = useState("");
+  const [AttendeesForm, setAttendeesForm] = useState("");
+  const [PublicPrivate, setPublicPrivate] = useState("");
+  const [HostName, setHostName] = useState("");
+  const [ContactForm, setContactForm] = useState("");
   function handleChange (e) {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
+const handleSubmit = async (e) => {
+  console.log("Hello")
+  e.preventDefault()
+try {
+let res = await fetch("http://localhost:3000/api/events/", {
+  method: "POST",
+  headers: { 'Content-Type': 'application/json' },
+  mode: "no-cors",
+  body: JSON.stringify({
+    event_title: EventTitle, date: DateForm, time: TimeForm, location: LocationForm, host_name: HostName, description: DescriptionForm, contact_info: ContactForm, public_private: PublicPrivate, max_attendees: parseInt(AttendeesForm) 
+  }),
+});
+console.log(res)
+} catch (err){console.log(err)}
+// let resJson = await res.json();
+//       if (res.status === 200) {console.log("resJson", resJson)}
+}
+console.log("EventTitle", EventTitle)
     return(
       <div className="wrapper"> 
         <Header/>
         <div className = "container">
           <div className = "form-container">
             <h2> Enter the details of your event:</h2>
-             
-            <form> 
+
+            <form onSubmit={handleSubmit}> 
             {/* Event title form */}
             <fieldset className = "input-box-fieldset"> {/* Creates box around the text and input box */}
               <h4>Event title</h4>
-              <input title="title" className = "input-boxes" /> {/* Creates a box that takes an input, saves it into title */}
+              <input value={EventTitle} className = "input-boxes" onChange={(e) => {
+                console.log(e.target.value)
+                setEventTitle(e.target.value)}}/> {/* Creates a box that takes an input, saves it into title */}
             </fieldset> <br></br>
 
             {/* Date form */}
             <fieldset className = "input-box-fieldset"> 
               <h4>Date</h4>
-              <input date="date" className = "input-boxes" placeholder='(mm/dd/yyyy)'/> 
+              <input value={DateForm} className = "input-boxes" placeholder='(mm/dd/yyyy)' onChange={(e) => setDateForm(e.target.value)}/> 
             </fieldset> <br></br>
 
             {/* Time form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Time</h4>
-                <input time="time" className = "input-boxes"/>
+                <input value={TimeForm} className = "input-boxes" onChange={(e) => setTimeForm(e.target.value)}/>
             </fieldset> <br></br>
 
             {/* Location form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Location</h4>
-                <input location="location" className = "input-boxes"/>
+                <input value={LocationForm} className = "input-boxes" onChange={(e) => setLocationForm(e.target.value)}/>
             </fieldset> <br></br>
 
             {/* Description form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Description</h4>
-                <input description="description" className = "input-boxes"/>
+                <input value={DescriptionForm} className = "input-boxes" onChange={(e) => setDescriptionForm(e.target.value)}/>
             </fieldset> <br></br>
 
 
-            {/* # of attendees form */}
+            {/* Max # of attendees form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Max number of attendees</h4>
-                <input numAttendees="numAttendees" className = "input-boxes"/>
+                <input value={AttendeesForm} className = "input-boxes" onChange={(e) => setAttendeesForm(e.target.value)}/>
             </fieldset> <br></br>
 
             {/* public/private form */}
             <fieldset className = "pub-priv-box-fieldset">
                 <h4>Public/Private</h4>
-                <select><option>Public</option><option>Private</option></select>
+                <select value={PublicPrivate} onChange={(e) => setPublicPrivate(e.target.value)}>
+                 <option name="public" value="public" > public</option>
+                 <option name="private" value="private" >private</option>
+                </select>
+                
             </fieldset> <br></br>
 
             {/* Host name form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Host name</h4>
-                <input hostName="hostName" className = "input-boxes"/>
+                <input value={HostName} className = "input-boxes" onChange={(e) => setHostName(e.target.value)}/>
             </fieldset> <br></br>
 
             {/* Contact info form */}
             <fieldset className = "input-box-fieldset">
                 <h4>Contact info</h4>
-                <input contact="contact" className = "input-boxes"/>
+                <input value={ContactForm} className = "input-boxes" onChange={(e) => setContactForm(e.target.value)}/>
             </fieldset> <br></br><br></br>
-
+            <button type="submit">Publish</button>
             </form>
           </div>
+
           <div className='upload-container'>
             {/* Upload an image */}
             {/* fieldset style creates a gray box */}
@@ -85,7 +119,7 @@ function CreateEvent() {
             <br></br><br></br> 
               <div>
                 {/* Button to publish draft */}
-                <button type="publish" className = "publish-button">Publish</button>
+
 
                 {/* Button to save draft (if have time) */}
                 <button type="save draft" className = "save-button">Save draft</button>
